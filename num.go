8230f2883ea950dab1c965a478d3e2b427e0bd29
dictvm <dh@horrendum.de>
2013-11-25 02:54:09 +0100

    added division by 5, added writing of results to hardcoded file

--- num.go
+++ num.go
@@ -2,6 +2,8 @@ package main
 
 import (
     "fmt"
+    "os"
+    "strconv"
 )
 
 func main() {
@@ -18,7 +20,30 @@ func main() {
     for count != inputNumber {
         count++
         if count%5 == 0 {
-            fmt.Println(count)
+// no longer print to stdout, let's write to /tmp/nondiv.txt instead!
+//            fmt.Println(count)
+        writeFile(count)
         }
+    }
+}
+
+func writeFile(num int) {
+    fmt.Println("Writing non-divideable numbers to file: ")
+    f, err := os.OpenFile("/tmp/nondiv.txt", os.O_APPEND | os.O_WRONLY | os.O_CREATE, 0666)
+    if err != nil {
+        fmt.Println("Error #1: Couldn't write to file.")
+        fmt.Println(err)
+    }
+    n, err := f.WriteString(strconv.Itoa(num) + "\n")
+    if err != nil {
+        fmt.Println("Error #2: Couldn't write fo file.")
+        fmt.Println(n, err)
+    }
+    f.Close()
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
     }
 }
